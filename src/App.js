1d280a39f16acbab28e1d373a5c3c9572ba9907f
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-12-29 23:52:39 +0900

    feat : redux의 islogin여부를 통한 라우터 추가

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import MyPage from './pages/MyPage/MyPage';
 import Budget from './pages/Budget/Budget';
 import CheckList from './pages/CheckList/CheckList';
 import Login from './pages/Login/Login';
-import Users from './pages/Users/Users';
 import KakaoRedirectHandler from './components/KakaoRedirectHandler.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  let state = useSelector((state) => state.users);
+
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/SubMain/:areaCode" element={<SubMain />} />
-      <Route path="/Plan/:areaCode" element={<Plan />} />
+      {state.isLogin ? <Route path="/Plan/:areaCode" element={<Plan />} /> : <Route path="/Plan/:areaCode" element={<Login />} />}
       <Route path="/list/:region/:type" element={<List />} />
       <Route path="/detail/:region/:contentid" element={<DetailPage />} />
       <Route path="/mypage/:nickName/:option" element={<MyPage />} />
-      <Route path="/Budget" element={<Budget />} />
-      <Route path="/CheckList" element={<CheckList />} />
+      {state.isLogin ? <Route path="/Budget" element={<Budget />} /> : <Route path="/Budget" element={<Login />} />}
+      {state.isLogin ? <Route path="/CheckList" element={<CheckList />} /> : <Route path="/CheckList" element={<Login />} />}
       <Route path="/login" element={<Login />} />
-      <Route path="/users" element={<Users />} />
       <Route path="/oauth/callback/kakao" element={<KakaoRedirectHandler />} />
     </Routes>
   );
