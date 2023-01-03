afaeee1f594f57e3f9c6485a867d8bdb7ed1c585
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-04 00:57:16 +0900

    design : review css 변경

--- src/pages/MyPage/MyReview.jsx
+++ src/pages/MyPage/MyReview.jsx
@@ -15,40 +15,42 @@ export default function MyReview({ data, nickName }) {
             <br></br>
             <span>리뷰✏️ 입니다</span>
           </h1>
-          {data.length === 0 ? (
-            <div>작성한 리뷰가 없습니다</div>
-          ) : (
-            data.map((a, i) => {
-              return (
-                <Container
-                  className="m-auto text-center col-12 col-md-10"
-                  style={{ fontSize: '12px' }}
-                  key={i}
-                >
-                  <Card className="mt-3">
-                    <Card.Body>
-                      <Card.Title className="mb-3 fs-6 bg-success text-light w-50 p-1 m-5 m-auto rounded">
-                        {a.title}
-                      </Card.Title>
-                      <div className="d-flex">
-                        <div className="border rounded w-50">
-                          <p className="mb-2 text-muted">{a.dateFull}</p>
-                          <Card.Text className="mb-2">
-                            {statArr.slice(0, a.star)}
-                          </Card.Text>
+          <Row className="d-flex w-75 m-auto">
+            {data.length === 0 ? (
+              <div>작성한 리뷰가 없습니다</div>
+            ) : (
+              data.map((a, i) => {
+                return (
+                  <Container
+                    className="m-auto text-center col-12 col-md-10"
+                    style={{ fontSize: '12px' }}
+                    key={i}
+                  >
+                    <Card className="mt-3">
+                      <Card.Body>
+                        <Card.Title className="mb-3 fs-6 bg-success text-light w-50 p-1 m-5 m-auto rounded">
+                          {a.title}
+                        </Card.Title>
+                        <div className="d-flex">
+                          <div className="border rounded w-50">
+                            <p className="mb-2 text-muted">{a.dateFull}</p>
+                            <Card.Text className="mb-2">
+                              {statArr.slice(0, a.star)}
+                            </Card.Text>
+                          </div>
+                          <div className="w-50 ms-2 border rounded">
+                            <Card.Text className=" d-flex align-items-center justify-content-center h-100 fs-6">
+                              {a.content}
+                            </Card.Text>
+                          </div>
                         </div>
-                        <div className="w-50 ms-2 border rounded">
-                          <Card.Text className=" d-flex align-items-center justify-content-center h-100 fs-6">
-                            {a.content}
-                          </Card.Text>
-                        </div>
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </Container>
-              );
-            })
-          )}
+                      </Card.Body>
+                    </Card>
+                  </Container>
+                );
+              })
+            )}
+          </Row>
         </Row>
       </Tab.Pane>
     </>
