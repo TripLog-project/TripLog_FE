2835b230b12dcc9cab5ec2bd2f442a18084d6ddc
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2023-01-03 23:26:32 +0900

    refactor : 로그인에 필요하지 않는 유효성 검사 제거

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,27 +8,15 @@ import Forminput from '../../components/Forminput';
 import Btn from '../../components/Button';
 import { login } from '../../store/modules/users';
 
-const ERROR_MSG = {
-  required: '필수 정보입니다.',
-  invalidUserEmail: '@ 를 사용하세요',
-  invalidUserPW: '8자 이상 영문, 숫자를 사용하세요.',
-};
-
 // 카카오톡 로그인 필요 정보, CLIENT_ID 로 REST API 키 사용 필요
 const KAKAO_CLIENT_ID = 'f25833457b45f3935443a269e01a48b1';
 const KAKAO_REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao';
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 export default function Login() {
-  const [useremail, setUseremail] = useState('');
-  const [userpw, setUserpw] = useState('');
-  const [errorMsg] = useState(ERROR_MSG);
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [UserEmailValid, setUserEmailValid] = useState(false);
-  const [UserPwValid, setUserPwValid] = useState(false);
   const [fixEmailValue, setFixEmailValue] = useState('thals0@gmail.com');
   const [fixPwValue, setFixpwValue] = useState('11111111aa');
 
@@ -38,27 +26,6 @@ export default function Login() {
 
   const pwValueChange = (e) => {
     setFixpwValue(e.target.value);
-  };
-
-  const handleEmail = (e) => {
-    setUseremail(e.target.value);
-    const USEREMAIL_REGEX =
-      /^([0-9a-zA-Z_.-]+)@([0-9a-zA-Z_-]+)(.[0-9a-zA-Z_-]+){1,2}$/;
-    if (USEREMAIL_REGEX.test(useremail)) {
-      setUserEmailValid(true);
-    } else {
-      setUserEmailValid(false);
-    }
-  };
-
-  const handlePw = (e) => {
-    setUserpw(e.target.value);
-    const USERPW_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/;
-    if (USERPW_REGEX.test(userpw)) {
-      setUserPwValid(true);
-    } else {
-      setUserPwValid(false);
-    }
   };
 
   async function loginUser() {
@@ -121,11 +88,6 @@ export default function Login() {
               type: 'text',
               placeholder: 'test@gmail.com',
             }}
-            validText={
-              !UserEmailValid && useremail.length > 0
-                ? errorMsg.invalidUserEmail
-                : null
-            }
           />
           <Forminput
             id="userpw"
@@ -139,9 +101,6 @@ export default function Login() {
               type: 'password',
               placeholder: '영문, 숫자 포함 8글자 이상',
             }}
-            validText={
-              !UserPwValid && userpw.length > 0 ? errorMsg.invalidUserPW : null
-            }
           />
           <Btn
             // onClick={() => {(checkUser())}}
